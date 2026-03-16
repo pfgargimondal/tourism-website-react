29472090d41af28ad3f96fb93e098648ae199023
Avbdgvx <pfgargimondal@gmail.com>
2026-03-16 19:33:23 +0530

    first commit

--- src/pages/PrivacyPolicy/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy/PrivacyPolicy.js
@@ -2,6 +2,7 @@ import "./PrivacyPolicy.css";
 import http from "../../http";
 import Loader from "../../component/Loader/Loader";
 import { useEffect, useState } from "react";
+import { PolicyComponent } from "../PolicyComponent/PolicyComponent";
 export const PrivacyPolicy = () => {
 
     const [loading, setLoading] = useState(false);
@@ -28,32 +29,7 @@ export const PrivacyPolicy = () => {
         {loading && <Loader/>}
         
       <div className="legal-pages-wrapper">
-        <div className="diwenrwer">
-            {privacyPolicyDetails.data?.banner_image && (
-                <img
-                src={`${privacyPolicyDetails.image_url}/${privacyPolicyDetails.data.banner_image}`}
-                className="img-fluid"
-                alt=""
-                />
-            )}
-        </div>
-
-        <div className="cuisdjrwerwer py-5">
-            <div className="container">
-                <h1>{privacyPolicyDetails.data?.title &&
-              privacyPolicyDetails.data.title}</h1>
-
-                <hr style={{ background: "linear-gradient(45deg, var(--main-green-color), var(--blue-primary-color))", width: "5rem", paddingBottom: "5px", opacity: 1, borderRadius: "10px", borderTop: 0 }} />
-
-                <div
-                    dangerouslySetInnerHTML={{
-                    __html:
-                        privacyPolicyDetails.data?.description &&
-                        privacyPolicyDetails.data.description,
-                    }}
-                />
-            </div>
-        </div>
+        <PolicyComponent PolicyDetails={privacyPolicyDetails}/>
       </div>
       </>
     )
--- src/pages/TermsCondition/TermsCondition.js
+++ src/pages/TermsCondition/TermsCondition.js
@@ -2,6 +2,7 @@ import "./TermsCondition.css";
 import http from "../../http";
 import Loader from "../../component/Loader/Loader";
 import { useEffect, useState } from "react";
+import { PolicyComponent } from "../PolicyComponent/PolicyComponent";
 export const TermsCondition = () => {
 
     const [loading, setLoading] = useState(false);
@@ -28,32 +29,7 @@ export const TermsCondition = () => {
         {loading && <Loader/>}
         
       <div className="legal-pages-wrapper">
-        <div className="diwenrwer">
-            {termsConditionDetails.data?.banner_image && (
-                <img
-                src={`${termsConditionDetails.image_url}/${termsConditionDetails.data.banner_image}`}
-                className="img-fluid"
-                alt=""
-                />
-            )}
-        </div>
-
-        <div className="cuisdjrwerwer py-5">
-            <div className="container">
-                <h1>{termsConditionDetails.data?.title &&
-              termsConditionDetails.data.title}</h1>
-
-                <hr style={{ background: "linear-gradient(45deg, var(--main-green-color), var(--blue-primary-color))", width: "5rem", paddingBottom: "5px", opacity: 1, borderRadius: "10px", borderTop: 0 }} />
-
-                <div
-                    dangerouslySetInnerHTML={{
-                    __html:
-                        termsConditionDetails.data?.description &&
-                        termsConditionDetails.data.description,
-                    }}
-                />
-            </div>
-        </div>
+        <PolicyComponent PolicyDetails={termsConditionDetails}/>
       </div>
       </>
     )
