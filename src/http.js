e925e2cf59a6419f413126711f573c8cd5950ee5
Avbdgvx <pfgargimondal@gmail.com>
2026-03-19 19:04:48 +0530

    multipage changes commit

--- src/http.js
+++ src/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL:"http://travel-tourism-backend.workstream.club/api/",
+    baseURL:"https://travel-tourism-backend.workstream.club/api/",
     headers:{
         "Content-Type":"application/json"
     }
